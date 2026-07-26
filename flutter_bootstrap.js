81c044a8fe6ba2cae1849995c4aa9040ced79385
Nicolas Aubinet <n.aubinet@gmail.com>
2026-07-26 13:08:43 +0200

    Update with smartcube support

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3430770611"
+    serviceWorkerVersion: "2834871810"
   }
 });
